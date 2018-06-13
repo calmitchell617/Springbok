96fee07b15a84b0efb5554969665f05cda06c625
Cal Mitchell <calmitchell617@gmail.com>
2018-06-13 16:15:47 -0400

    Created earnings_growth.py, which makes a new csv file in fundamentals to show earnings growth.

---
+++ mkdirs.py
@@ -0,0 +1,14 @@
+import os
+
+# run this file first to make directories for everything
+
+os.mkdir('data_downloads')
+os.mkdir('backtest_outputs')
+os.mkdir('io_files')
+os.mkdir('processed_data')
+os.mkdir('io_files/companies')
+os.mkdir('processed_data/fundamentals')
+os.mkdir('processed_data/preprocessed')
+os.mkdir('processed_data/pricing')
+os.mkdir('processed_data/pricing/daily')
+os.mkdir('processed_data/pricing/preprocessed')
